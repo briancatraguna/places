c8e9f6252f1aa9c489bebed021108fcb6f9fba26
Brian Catraguna <briancatraguna@rocketmail.com>
2023-02-24 17:54:46 -0500

    Added navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import {
   BrowserRouter as Router,
   Route,
@@ -8,23 +7,25 @@ import {
 
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
   return (
     <Router>
-      <Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
 
-        <Route path="/" exact>
-          <Users />
-        </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
 
-        <Route path="/places/new" exact>
-          <NewPlace />
-        </Route>
-
-        <Redirect to="" />
-
-      </Switch>
+          <Redirect to="" />
+        </Switch>
+      </main>
     </Router>
   );
 };
