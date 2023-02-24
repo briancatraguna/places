e715c5ffad2f4046ea2f82576cccc5939f64596b
Brian Catraguna <briancatraguna@rocketmail.com>
2023-02-24 16:12:57 -0500

    Create routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 
-function App() {
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+
+const App = () => {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Switch>
+
+        <Route path="/" exact>
+          <Users />
+        </Route>
+
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+
+        <Redirect to="" />
+
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
